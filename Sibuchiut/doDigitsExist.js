ae6c3735058a291c31c9e35803b46b1fa6249492
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-01-30 20:20:59 +0200

    Didnt succeed to solve SIbuchiut exercise

---
+++ Sibuchiut/doDigitsExist.js
@@ -0,0 +1,9 @@
+// let num = 456;
+    
+// function doDigitsExist(someNum){
+//     const numArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+//     let temp = someNum;
+//     while(temp > 0){
+
+//     }
+// }
