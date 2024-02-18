7f45fdc4c4f4c46776b1d51f166275f55c087520
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-02-18 15:46:59 +0200

    Starts working

--- TypeScript/OOP/BankSystem.ts
+++ TypeScript/OOP/BankSystem.ts
@@ -23,7 +23,7 @@ Design a Bank system
 class Bank{
     public static kindsOfClients:String[] = ["regular", "gold", "platinum"];
     private _name!: String;
-    private _clients: Client[];
+    private _clients: Client[] = [];
     private _totalAccounts: number =0;
     private _totalMoney: number = 0;
     private _totalActivity: number = 0;
@@ -38,17 +38,33 @@ class Bank{
         }
         else{console.log("Name Cant be shorter than 2 character.");}
     }
+    getClientReport(id:String):void{
+        let clientInfo ;
+        for(let index =0; index < this._clients.length; index++){
+            if(this._clients[index].getClientID == id){
+                clientInfo = this._clients[index].getClientInfo();
+                console.log(clientInfo);
+                return;
+            }
+        }
+    }
     public addNewClient(name:String, id:String, typeOfClient: String){
         const newClient = new Client(name, id, typeOfClient);
-        newClient
+        this._clients.push(newClient);
+        newClient.addAccount();
+    }
+    public getReportAll(){
+        for(let index = 0; index < this._clients.length; index++){
+            console.log(this._clients[index].getClientInfo());
+        }
     }
 }
 class Client{    
     private _name!: String;
     private _id!: String;
     private _typeOfClient!: String;
-    private _accountsList!: Account[];
-    private _totalAccounts!: number;
+    private _accountsList: Account[] = [];
+    private _totalAccounts: number =0;
 
     constructor(name: String, id: String, clientType: String){
         this.setName = name;
@@ -86,10 +102,41 @@ class Client{
             this._id = value;
         }
     }
-    public addAccount(){
-
+    get getTotalMoney():number{
+        let totalMoneyClient = 0;
+        for(let index =0; index< this._accountsList.length; index++){
+            totalMoneyClient += this._accountsList[index].getAccountMoney;
+        }
+        return totalMoneyClient;
     }
-
+    get getClientID():String{
+        return this._id;
+    }
+    public addAccount(){
+        let newAccount 
+        if(this._accountsList.length ==0){
+            let nodeId = 1;
+            newAccount = new Account(nodeId, this._id);
+            this._accountsList.push(newAccount);
+            this._totalAccounts++;
+            return;
+        }
+        newAccount = new Account(this._accountsList.length, this._id);
+        this._totalAccounts ++;
+    }
+    getAccountsReport():String{
+        let totalAccountsReport ='';
+        for(let index =0; index < this._accountsList.length; index++){
+            totalAccountsReport+= `${this._accountsList[index].getAccountInfo}\n`;           
+        }
+        return `Hello ${this._name}, you have ${this._totalAccounts} accounts.\n 
+        Total sum: ${this.getTotalMoney}\n
+        ${totalAccountsReport}
+        `;
+    }
+    getClientInfo():String{
+        return`Name:${this._name}, ID: ${this._id}\n Total money: ${this.getTotalMoney}`;
+    }
 
 }
 class Account{
@@ -97,7 +144,7 @@ class Account{
     private _clientId!: String;
     private _totalMoney: number =0;
 
-    constructor(nodeId: number, clientId: string){
+    constructor(nodeId: number, clientId: String){
         this._nodeId = nodeId;
         this._clientId = clientId;
     }
@@ -110,5 +157,24 @@ class Account{
         if(value > 0){
             this._totalMoney -= value;
         }
+    }
+    get getAccountMoney():number{
+        return this._totalMoney;
+    }
+    get getNodeID():number{
+        return this._nodeId;
     }    
+    get getClientID():String{
+        return this._clientId;
+    }
+    get getAccountInfo():String{
+        return `Account Id ${this._nodeId}-${this._clientId}, Total money: ${this._totalMoney}`;
+    }
 }
+
+
+const bankPoalim = new Bank("Hapoalim");
+bankPoalim.addNewClient('Yaroslav', "320691660", 'regular');
+bankPoalim.addNewClient('Vadim', "217555455", "gold")
+console.log(bankPoalim.getClientReport('320691660'));
+bankPoalim.getReportAll();
