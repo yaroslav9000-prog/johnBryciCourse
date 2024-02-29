4add51464ecac4a082adf8d3fe80ecf8e92a6593
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-03-01 00:05:49 +0200

    look here

--- project2/coinsPage.js
+++ project2/coinsPage.js
@@ -1,1 +1,29 @@
+const coinArray = [];
+const createCard = (someCoin)=>{
+    const cardBody = `<div class="card col-4">
+    <div class="card-body">
+      <div class="d-flex justify-content-between">
+        <h4 class="card-title">${someCoin.id}</h4>
+        <label class="switch">
+          <input type="checkbox" />
+          <span class="slider round"></span>
+        </label>
+      </div>
+      <h6 class="card-subtitle mb-2 text-body-secondary">${someCoin.name}</h6>
+      <button class="btn btn-primary mt-3 border-dark">
+        More info
+      </button>
+    </div>
+  </div>`;
+    document.getElementById('coinsContainer').innerHTML += cardBody;
+}
+
+document.addEventListener('event:coins',(event)=>{
+    console.log(event.detail);
+    createCard(event.detail);
+    
+    
+})
+
+
 
