8fcbc57813c57d283a5a6417375895d7d25a9e2e
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-03-14 20:14:32 +0200

    so so

--- project2/index.js
+++ project2/index.js
@@ -1,7 +1,9 @@
 const timeInterval =120000;
 const myDate = new Date();
-
-$(()=>{
+const trashIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+</svg>`;
   let firstImg = '<div class="first-image" ></div>';
   let aboutPage = `<div class="container-fluid col-xxl-8 px-5 py-5 first-image"><div class="row flex-lg-row-reverse align-items-center g-5 py-5"><div class="col-10 col-sm-8 col-lg-6"><img src="./pictures/passportPic.jpg" class="d-block mx-lg-auto img-fluid border border-dark border-5 rounded-pill" alt="Bootstrap Themes" width="300"height="200"loading="lazy"/></div><div class="col-lg-6 bg-dark p-2 rounded-3 border-3 border-dark"><h1 class="display-5 fw-bold text-white lh-1 mb-3">Yaroslav Yarkevich</h1><p class="lead text-light">Hey there, it's me Yaroslav it's my second project in John Bryce Academy. In this project we pratice AJAX api reponses and else. Work with HTML.</p></div></div></div>`; 
          
@@ -21,16 +23,16 @@ $(()=>{
   const createCard = (currentCoin)=>{
     
        
-    const cardBody = `<div class="card col-md-4 col-9">
+    const cardBody = `<div  class="card col-md-4 col-9 ">
       <div class="card-body">
         <div class="d-flex justify-content-between">
           <h4 class="card-title">${currentCoin.id}</h4>
           <label class="switch" >
-            <input type="checkbox" onclick="addCoin('${currentCoin.id}')"/>
+            <input id="${currentCoin.id}CheckBox" type="checkbox" onclick="checkList('${currentCoin.id}')"/>
             <span class="slider round"></span>
           </label>
         </div>
-        <h6 class="card-subtitle mb-2 text-body-secondary">${currentCoin.name}</h6>
+        <h6 class="card-subtitle mb-2 text-body-secondary ">${currentCoin.name}</h6>
         <button
         type="button"
         class="btn btn-primary justify-content-between"
@@ -45,7 +47,7 @@ $(()=>{
   
   <!-- Modal -->
   <div
-                  class="modal fade"
+                  class="modal fade "
                   id="${currentCoin.id}"
                   data-bs-backdrop="static"
                   data-bs-keyboard="false"
@@ -131,30 +133,41 @@ $(()=>{
   
     
 loadCoinList();
-    
-// let mainPage = `<header class="d-flex flex-wrap justify-content-center py-3 bg-dark"><a href="" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"><svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg><span class="fs-3 text-light">Crypto Report</span></a><ul class="nav nav-pills"><li class="nav-item"><a href="#" class="nav-link">Coins</a></li><li class="nav-item"><a href="#" class="nav-link">Real time report</a></li><li class="nav-item"><a href="#" class="nav-link">About</a></li></ul></header><div id="content"><div class="first-image" id=""><div class="bg-dark p-5 mb-5 rounded"><p class="text-light">Crypto Report is a website that provides the latest news and analysis on the cryptocurrency market. Whether you are a beginner or an expert, you can find useful information and insights on Crypto Report. You can also check the live prices of various cryptocurrencies and compare them with different fiat currencies. Crypto Report is your source for everything crypto.</p></div></div><div class="container-fluid py-4 bg-dark"><p class="fs-5 text-light">A cryptocurrency, crypto-currency, or crypto is a digital currency designed to work as a medium of exchange through a computer network that is not reliant on any central authority, such as a government or bank, to uphold or maintain it.</p></div><div class="d-flex justify-content-center" id="second-image"><div class="px-5 py-2 text-success bg-dark rounded"><em><p class="fs-3">Start making Money</p></em></div></div><div class="container-fluid py-4 bg-dark"><p class="fs-5 text-light">We offer you a great platform for tracking and creating your own portfolio. So start doing your trading with us.<span class="text-success" id="coinsLink">Start</span></p></div></div>`;
 
-   
 
-})
+
+
 
 const myCoins = [];
-const addCoin = (currentId)=>{    
-
-  const idPosition = myCoins.indexOf(currentId);
-  if(myCoins.length < 5){
-  if(idPosition < 0){
-    myCoins.push(currentId);
-    console.log(myCoins);
+const myModal = $('#myModal');
+//Working with 5 coins.
+const checkList =(currentId)=>{
+  if(myCoins.length <5){
+    addCoin(currentId);
+  }else{
+    $(`#${currentId}CheckBox`).prop('checked', false);
+    $('#myModalBody').empty();
+    for(let index = 0; index < myCoins.length; index++){
+      $('#myModalBody').append(`<div class="d-flex justify-content-between">${myCoins[index]} <button type='button' onclick="addCoin('${myCoins[index]}')" class='btn btn-danger border border-0'">${trashIcon}</button></div>`)
+    }
+    myModal.modal('show');
+    return;
+    
   }
 }
-  if(idPosition >=0){
-  myCoins.splice(idPosition, 1);
-  console.log(myCoins);
+const addCoin = (currentId)=>{    
+  const currentCoin = myCoins.indexOf(currentId);
+  if(currentCoin < 0){
+    myCoins.push(currentId);
+  }else{
+    myCoins.splice(currentCoin, 1);
+    $(`#${currentId}CheckBox`).prop('checked', false);
   }
-}  
+}
+//
 
 
+//Checking how long ago coin info was updated. And Building modal body for each one of them.
 async function checkCoin(currentId){
   const currentCoin = JSON.parse(localStorage.getItem(`${currentId}`));
   if(currentCoin !== null){
@@ -183,5 +196,7 @@ const buildModal = (currentId, data)=>{
     .then(result => {result.lastUpdated = myDate.getTime(); localStorage.setItem(`${currentId}`, JSON.stringify(result)); buildModal(currentId, result)})    
     .catch(error=> console.log('error: ', error));
     }
-
-    
+const coinVal = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD";
+$.getJSON(coinVal, (data)=>{
+  console.log(data);
+})
