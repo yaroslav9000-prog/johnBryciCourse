0cfa65a0c96d3d0a2e8c5d63a5e302ec01444578
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-03-28 21:44:18 +0200

    finished tictactoe

--- react/tic-tac-toe/src/Components/Layout/Board/Board.tsx
+++ react/tic-tac-toe/src/Components/Layout/Board/Board.tsx
@@ -10,14 +10,48 @@ function Board(): JSX.Element {
     function handleClick(i : number):any{
         //this function is passed down to square component with argument of id 
         //we copy current array, update the array at passed index and update and set new state. 
+        //in order to stop changing existing i need to check if value exists at specific position.
+        //if does exist stop updating state.
         const nextSquares = squares.slice();
+        if(nextSquares[i] || calculateWinner(squares)){
+            return;
+        }else{
         nextSquares[i] = xIsNext? "X": "O";
         setSquares(nextSquares);
         setXNext(!xIsNext);
+        }
     }
-
+    
+    const calculateWinner = (squares: String[]) :String =>{
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+        for(let i:number = 0; i < lines.length; i++){
+            const [a, b, c] = lines[i];
+            if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+                return squares[a];
+            }
+        }
+        return '';
+    }
+    const winner = calculateWinner(squares);
+    let status;
+    if(winner){
+        status = 'Winner is :' + winner;
+    }else{
+        status = 'Next player: ' + (xIsNext? 'X': 'O');
+    }
     return (
+        
         <div className="Board">
+            <div className='status' >{status}</div>
             <div className="row">
                 <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
                 <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
@@ -36,6 +70,7 @@ function Board(): JSX.Element {
 
         </div>
     );
+    
 }
 
 export default Board;
